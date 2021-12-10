6310f475a6d03f84cc17556e1e45e1d256fbd0b2
Jose Maria Valera Reales <jose.valera@teufel.de>
2021-12-10 19:37:15 +0100

    Refactoring Calculator: extracting methods

--- php/src/StringCalculator.php
+++ php/src/StringCalculator.php
@@ -12,19 +12,34 @@ final class StringCalculator
      */
     public function add(string $numbers): int
     {
-        $separator = ',';
-        if (0 === strpos($numbers, '//')) {
-            $separator = $numbers[2];
-        }
+        $separator = $this->extractSeparator($numbers);
 
         $normalizedNumbers = str_replace(["\n", '//'], $separator, $numbers);
         $explodedNumbers = explode($separator, $normalizedNumbers);
-        $negativeNumbers = array_filter($explodedNumbers, fn(string $number) => (int) $number < 0);
+
+        $this->validateOnlyPositiveNumbers($explodedNumbers);
+
+        return array_sum($explodedNumbers);
+    }
+
+    private function validateOnlyPositiveNumbers(array $explodedNumbers): void
+    {
+        $negativeNumbers = array_filter(
+            array_filter($explodedNumbers),
+            fn(string $number) => (int)$number < 0
+        );
 
         if (!empty($negativeNumbers)) {
             throw NumbersNotAllowedException::withNegativeNumbers($negativeNumbers);
         }
+    }
 
-        return array_sum($explodedNumbers);
+    private function extractSeparator(string $numbers): string
+    {
+        if (0 === strpos($numbers, '//')) {
+            return $numbers[2];
+        }
+
+        return ',';
     }
 }
