ec582ce250a28f762132d516ac50d6a2b9ed5b1d
Pascual <pascual@strata3.com>
2021-12-10 18:53:20 +0100

    Refactor. Introducing map and reduce.

--- javascript/src/add.fn.js
+++ javascript/src/add.fn.js
@@ -1,15 +1,9 @@
 const add = (input) => {
   if (input === "") return 0;
-  const numbers = input.split(",");
-  if (numbers.length === 1) {
-    return parseInt(input);
-  }
-  if (numbers.length === 2) {
-    return parseInt(numbers[0]) + parseInt(numbers[1]);
-  }
-  if (numbers.length === 3) {
-    return parseInt(numbers[0]) + parseInt(numbers[1]) + parseInt(numbers[2]);
-  }
+  return input
+    .split(",")
+    .map((number) => parseInt(number))
+    .reduce((acc, curr) => acc + curr, 0);
 };
 
 module.exports = { add };
