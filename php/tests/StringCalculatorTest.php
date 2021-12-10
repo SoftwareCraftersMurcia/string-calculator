516499937c16782745dd30acc6c112537ec22678
Jose Maria Valera Reales <jose.valera@teufel.de>
2021-12-10 18:40:56 +0100

    Refactoring Test: moving up the calculator to setUp()

--- php/tests/StringCalculatorTest.php
+++ php/tests/StringCalculatorTest.php
@@ -7,12 +7,17 @@ use PHPUnit\Framework\TestCase;
 
 final class StringCalculatorTest extends TestCase
 {
+    private StringCalculator $stringCalculator;
+
+    protected function setUp(): void
+    {
+        $this->stringCalculator = new StringCalculator();
+    }
+
     /** @test */
     public function add_empty_string_is_zero(): void
     {
-        $stringCalculator = new StringCalculator();
-
-        $result = $stringCalculator->add('');
+        $result = $this->stringCalculator->add('');
 
         self::assertSame(0, $result);
     }
@@ -20,9 +25,7 @@ final class StringCalculatorTest extends TestCase
     /** @test */
     public function add_single_number_equal_same_number(): void
     {
-        $stringCalculator = new StringCalculator();
-
-        $result = $stringCalculator->add('4');
+        $result = $this->stringCalculator->add('4');
 
         self::assertSame(4, $result);
     }
